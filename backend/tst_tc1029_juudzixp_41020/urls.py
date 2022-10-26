45ec7efccaeb1b4e41d8509609aef65b221ee7e6
Crowdbotics <engineering@crowdbotics.com>
2022-10-26 18:30:53 +0000

    Update admin title

--- backend/tst_tc1029_juudzixp_41020/urls.py
+++ backend/tst_tc1029_juudzixp_41020/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "TST-TC1029-juudzixpzs"
-admin.site.site_title = "TST-TC1029-juudzixpzs Admin Portal"
-admin.site.index_title = "TST-TC1029-juudzixpzs Admin"
+admin.site.site_header = "updatedName1029scmwpxxezn"
+admin.site.site_title = "updatedName1029scmwpxxezn Admin Portal"
+admin.site.index_title = "updatedName1029scmwpxxezn Admin"
 
 # swagger
 api_info = openapi.Info(
